b2b91d02d6868421c222971dcec092ca41693421
Raheel Bennett <raheel.bennett@telus.com>
2023-01-23 17:37:06 -0700

    create player class

---
+++ player.rb
@@ -0,0 +1,16 @@
+=begin
+player: will holder all information relater to the player, like name and score. and will update the score as well. 
+-name
+-score
+-attr_accessor : name :score
+=end
+
+class Player
+  attr_accessor :name, :score
+
+  def initialize(name)
+    @name = name
+    @score = 0
+  end
+
+end
