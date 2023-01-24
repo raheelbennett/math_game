ecd96eb58bd110c815f66ce64fce95900cbdac37
Raheel Bennett <raheel.bennett@telus.com>
2023-01-24 01:22:52 -0700

    add Game class and run test in main.rb

--- main.rb
+++ main.rb
@@ -8,12 +8,3 @@ player2 = Player.new("Player 2")
 game = MathQuestion.new(1, 20)
 newGame = Game.new(player1, player2, game, 3)
 newGame.play
-=begin
-MathQuestion
-
-=end
-
-# question = MathQuestion.new(1, 20)
-# puts question.newQuestion
-# answer = gets.chomp
-# puts question.verifyAnswer(answer)
